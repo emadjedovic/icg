966180222c33a4ba6008f307907b8ce3abfbbd1c
Ema <ema.djedovic27@gmail.com>
2025-04-03 22:50:59 +0200

    update

--- ICG_04/window.h
+++ ICG_04/window.h
@@ -37,6 +37,25 @@ MyPoint polygon[500];
 MySide sides[500];
 MyDiagonal diagonals[500];
 
+int Area2(const MyPoint &, const MyPoint &, const MyPoint &);
+int AreaPoly2(int, const MyPoint[100]);
+bool XOR(bool, bool);
+bool IntersectProp(
+    const MyPoint &, const MyPoint &, const MyPoint &, const MyPoint &);
+bool Left(const MyPoint &, const MyPoint &, const MyPoint &);
+bool LeftOn(const MyPoint &, const MyPoint &, const MyPoint &);
+bool Collinear(const MyPoint &, const MyPoint &, const MyPoint &);
+void SubVector(const MyPoint &, const MyPoint &, MyPoint &);
+int Dot(const MyPoint &, const MyPoint &);
+int Length2(const MyPoint &);
+bool IsPointOnSegment(const MyPoint &, const MyPoint &, const MyPoint &);
+bool Intersect(MyPoint, MyPoint, MyPoint, MyPoint);
+bool Diagonalie(int, int, int, MyPoint[100]);
+bool Diagonal(int, int, int, MyPoint[100]);
+void PointAssign(MyPoint*, MyPoint*);
+bool InCone(int, int, int, MyPoint[100]);
+void ClipEar(int, int, MyPoint[100]);
+
 //---------------------------------------------------------------------------
 extern PACKAGE TTriangulation* Triangulation;
 //---------------------------------------------------------------------------
